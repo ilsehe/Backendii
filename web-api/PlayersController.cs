50426c9d4ea119433e8ad676ca636f9fd3b3876d
ilsehe <43143411+ilsehe@users.noreply.github.com>
2018-09-10 17:59:16 +0300

    Update PlayersController.cs

--- web-api/PlayersController.cs
+++ web-api/PlayersController.cs
@@ -43,38 +43,6 @@ namespace web_api
         public Task<Player> Delete(Guid id)
         {
             return _processor.Delete(id);
-        }
-
-        // [Route("api/players")]
-        // [HttpPost]
-        // public Task<Player> Create(NewPlayer player)
-        // {
-        //     return _processor.Create(player);
-        // }
-
-        // [Route("api/players/{playerName}")]
-        // [HttpPost]
-        // public Task<Player> Create(string playerName)
-        // {
-        //     NewPlayer player = new NewPlayer(playerName);
-        //     return _processor.Create(player);
-        // }
-
-        // [Route("api/players/diiba")]
-        // [HttpPost]
-        // public Task<Player> CreateDiiba()
-        // {
-        //     NewPlayer player = new NewPlayer("Diiba");
-        //     return _processor.Create(player);
-        // }
-
-        // [Route("api/players")]
-        // [HttpPut]
-        // public Task<Player> Modify(Guid id, ModifiedPlayer player)
-        // {
-        //     return _processor.Modify(id, player);
-        // }
-
-        
+        }    
     }
 }
